de957e63fecb2e98764963620738c66cc58e3046
Raav Koper <rkoper@f1r5s18.codam.nl>
2022-11-28 11:21:10 +0100

    finished ex00

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,6 +1,7 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap() {
+	std::cout << "Constructer called" << std::endl;
 	this->name = "Trappy";
 	this->healthp = 10;
 	this->energyp = 10;
@@ -8,6 +9,7 @@ ClapTrap::ClapTrap() {
 }
 
 ClapTrap::ClapTrap(std::string name) {
+	std::cout << "Constructer called" << std::endl;
 	this->name = name;
 	this->healthp = 10;
 	this->energyp = 10;
@@ -15,6 +17,7 @@ ClapTrap::ClapTrap(std::string name) {
 }
 
 ClapTrap::ClapTrap(const ClapTrap &other) {
+	std::cout << "Copy Constructer called" << std::endl;
 	this->name = other.name;
 	this->healthp = other.healthp;
 	this->energyp = other.energyp;
@@ -22,6 +25,7 @@ ClapTrap::ClapTrap(const ClapTrap &other) {
 }
 
 ClapTrap &ClapTrap::operator=(const ClapTrap other) {
+	std::cout << "Assignment Operator called" << std::endl;
 	this->name = other.name;
 	this->healthp = other.healthp;
 	this->energyp = other.energyp;
@@ -29,7 +33,9 @@ ClapTrap &ClapTrap::operator=(const ClapTrap other) {
 	return (*this);
 }
 
-ClapTrap::~ClapTrap() {};
+ClapTrap::~ClapTrap() {
+	std::cout << "Destructor called" << std::endl;
+};
 
 void ClapTrap::attack(const std::string &target) {
 	if (this->energyp > 0 && this->healthp > 0) {
@@ -38,6 +44,27 @@ void ClapTrap::attack(const std::string &target) {
 	}
 	if (this->energyp == 0)
 		std::cout << "no energy left for this move" << std::endl;
-	if (this->healthp == 0)
+	if (this->healthp <= 0)
 		std::cout << "no health points left for this move" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+	if (this->energyp > 0 && this->healthp > 0) {
+		std::cout << "ClapTrap " << this->name << " has gained " << amount << " hp!" << std::endl;
+		this->energyp--;
+		this->healthp += amount;
+	}
+	if (this->energyp == 0)
+		std::cout << "no energy left for this move" << std::endl;
+	if (this->healthp <= 0)
+		std::cout << "no health points left for this move" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	if (this->healthp > 0) {
+		std::cout << "ClapTrap " << this->name << " has taken " << amount << " damage:(" << std::endl;
+		this->healthp -= amount;
+	}
+	else if (this->healthp <= 0)
+		std::cout << "Mans already dead chill.." << std::endl;
 }
---
+++ ex00/main.cpp
@@ -0,0 +1,8 @@
+#include "ClapTrap.hpp"
+
+int main(void) {
+	ClapTrap a;
+	ClapTrap b("Flapdrol");
+
+	
+}
